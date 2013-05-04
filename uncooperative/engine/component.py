bb9d4b10d726960a16f386f68a664a938afc83b0
Daniel Lister <dan.lister@gmail.com>
2013-05-04 12:24:39 -0400

    Basic tile stuff

--- uncooperative/engine/component.py
+++ uncooperative/engine/component.py
@@ -1,4 +1,5 @@
 import game
+import pygame
 
 class ExampleComponent(object):
     
@@ -37,3 +38,15 @@ class InputMovementComponent(object):
     
     def handle_update(self, entity, event):
         print 'You moved %s' % (event,)
+
+class TileDraw(object):
+    
+    def add(self, entity):
+        entity.register_handler('draw-tiles', self.handle_update)
+        game.get_game().register_for_updates(entity)
+    
+    def remove(self, entity):
+        entity.unregister_handler('draw-tiles', self.handle_update)
+        
+    def handle_update(self, entity, surface):
+        surface.blit(game.get_game().resource_manager.get('image', entity.props.image),(entity.props.x,entity.props.y))
