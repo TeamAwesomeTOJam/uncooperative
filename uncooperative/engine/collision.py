4f8adf0ae1fe84f879338476733e16194a8da748
Jonathan Doda <jonathan@jdoda.ca>
2013-05-04 17:17:56 -0400

    Use sets for grid squares instead of lists.

--- uncooperative/engine/collision.py
+++ uncooperative/engine/collision.py
@@ -19,33 +19,37 @@ class CollisionGrid(object):
     
     def add_entity(self, entity):
         for square in self.get_grid_squares_for_entity(entity):
-            self.map.setdefault(square, []).append(entity)
+            self.map.setdefault(square, set()).add(entity)
                 
     def remove_entity(self, entity):
         for square in self.get_grid_squares_for_entity(entity):
-            self.map.setdefault(square, []).remove(entity)
+            self.map.setdefault(square, set()).remove(entity)
     
     def get_possible_collisions(self, rect):
-        possible_collisions = []
+        possible_collisions = set()
         for square in self.get_grid_squares(rect):
             if square in self.map:
-                possible_collisions += self.map[square]
+                possible_collisions.update(self.map[square])
         return possible_collisions
     
     def get_possible_collisions_for_entity(self, entity):
-        return self.get_possible_collisions((entity.props.x, entity.props.y, entity.props.width, entity.props.height))
+        possible_collisions = self.get_possible_collisions((entity.props.x, entity.props.y, entity.props.width, entity.props.height))
+        possible_collisions.discard(entity)
+        return possible_collisions
     
     def get_collisions(self, rect):
         possible_collisions = self.get_possible_collisions(rect)
-        collisions = []
+        collisions = set()
         for entity in possible_collisions:
             entity_rect = (entity.props.x, entity.props.y, entity.props.width, entity.props.height)
             if self.rects_collide(rect, entity_rect):
-                collisions.append(entity)
+                collisions.add(entity)
         return collisions
              
     def get_collisions_for_entity(self, entity):
-        return self.get_collisions((entity.props.x, entity.props.y, entity.props.width, entity.props.height))
+        collisions = self.get_collisions((entity.props.x, entity.props.y, entity.props.width, entity.props.height))
+        collisions.discard(entity)
+        return collisions
     
     def rects_collide(self, a, b):
         a_x, a_y, a_w, a_h = a
