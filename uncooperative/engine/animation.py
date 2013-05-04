6527f147f9a91e594425c375a39a5323fdb0a57e
Jonathan Doda <jonathan@jdoda.ca>
2013-05-04 19:56:21 -0400

    Push a missing file.

---
+++ uncooperative/engine/animation.py
@@ -0,0 +1,40 @@
+'''
+Created on May 4, 2013
+
+@author: jonathan
+'''
+
+import game
+
+
+class AnimationComponent(object):
+    
+    def add(self, entity):
+        entity.register_handler('update', self.on_update)
+        entity.register_handler('play-animation', self.on_play_animation)
+        game.get_game().register_for_updates(entity)
+        entity.props.animation_pos = 0
+        entity.props.current_animation = 'default'
+        
+    def remove(self, entity):
+        entity.remove_handler('update', self.handle_update)
+        
+    def on_update(self, entity, dt):
+        entity.props.animation_pos += dt
+        if entity.props.animation_pos >= entity.props.animations[entity.props.current_animation]['duration']:
+            if entity.props.animation_should_loop:
+                entity.props.animation_pos - entity.props.animations[entity.props.current_animation]['duration']
+            else:
+                entity.handle('animation-finished', entity.props.current_animation)
+                entity.props.current_animation = 'default'
+                entity.props.animation_pos = 0
+                entity.props.animation_should_loop = True
+        frame_number = int(entity.props.animation_pos / entity.props.animations[entity.props.current_animation]['duration'] * len(entity.props.animations[entity.props.current_animation]['frames']))
+        entity.props.image = entity.props.animations[entity.props.current_animation]['frames'][frame_number]
+        
+    def on_play_animation(self, entity, animation, loop=False):
+        entity.props.current_animation = animation
+        entity.props.animation_should_loop = loop
+        entity.animation_pos = 0
+        
+        
