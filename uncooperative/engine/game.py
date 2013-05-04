4e866e41ea61aa586447ca05cb68bbffa826948c
Daniel Lister <dan.lister@gmail.com>
2013-05-03 23:25:25 -0400

    simple camera control

--- uncooperative/engine/game.py
+++ uncooperative/engine/game.py
@@ -12,6 +12,7 @@ import pygame
 import componentmanager
 from entitymanager import EntityManager
 from resourcemanager import ResourceManager
+from camera import Camera
 
 _game = None
 
@@ -19,6 +20,9 @@ _game = None
 class Game(object):
     
     def __init__(self):
+        self.screen_size = (500,500)
+        self.world_size = (5000,5000)
+        
         pygame.init()
         
         self.clock = pygame.time.Clock()
@@ -30,6 +34,20 @@ class Game(object):
         
         self.entities_to_update = []
         
+        self.camera1 = Camera(500,500)
+        self.camera2 = Camera(500,500)
+        self.camera3 = Camera(500,500)
+        self.camera4 = Camera(500,500)
+        
+        self.cameras = [self.camera1,self.camera2,self.camera3, self.camera4]
+        self.camera_dirs = [0,0,0,0]
+        self.current_camera = 0
+        
+        self.world_surface = pygame.Surface(self.world_size)
+        for x in range(0,self.world_size[0],10):
+            for y in range(0,self.world_size[1],10):
+                pygame.draw.rect(self.world_surface,(255,0,0),(x,y,5,5))
+        
     def register_for_updates(self, entity):
         self.entities_to_update.append(entity)
         
@@ -37,9 +55,31 @@ class Game(object):
         while True:
             dt = self.clock.tick() / 1000.0
             for e in pygame.event.get():
-                 if e.type == pygame.QUIT: sys.exit()
+                if e.type == pygame.QUIT: sys.exit()
+                elif e.type == pygame.KEYDOWN:
+                    if e.key == pygame.K_TAB:
+                        self.current_camera = (self.current_camera +1)%4
+                    if e.key == pygame.K_a:
+                        self.camera_dirs[self.current_camera] = 1
+                elif e.type == pygame.KEYUP:
+                    if e.key == pygame.K_a:
+                        self.camera_dirs[self.current_camera] = 0
             for entity in self.entities_to_update:
                 entity.handle('update', dt)
+            
+            for i in range(4):
+                if self.camera_dirs[i]:
+                    self.cameras[i].x += 1
+            rect = pygame.Rect(0,0,250,250)
+            rect.center = (self.camera1.x,self.camera1.y)
+            self.screen.blit(self.world_surface,(0,0),rect)
+            rect.center = (self.camera2.x,self.camera2.y)
+            self.screen.blit(self.world_surface,(250,0),rect)
+            rect.center = (self.camera3.x,self.camera3.y)
+            self.screen.blit(self.world_surface,(0,250),rect)
+            rect.center = (self.camera4.x,self.camera4.y)
+            self.screen.blit(self.world_surface,(250,250),rect)
+            pygame.display.flip()
 
 
 def get_game():
