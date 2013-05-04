83f064b9eb70c726eb4420b95f8cf6a8c35ed326
Jonathan Doda <jonathan@jdoda.ca>
2013-05-04 16:29:54 -0400

    Resolve a conflict, maybe?

--- uncooperative/engine/render.py
+++ uncooperative/engine/render.py
@@ -3,7 +3,8 @@
 import pygame
 from random import randint
 from camera import Camera
-
+from gridgen import GridGenerator
+from entity import Entity
 
 
 class Render:
@@ -15,9 +16,22 @@ class Render:
         self.screen_size = game.screen_size
         self.world_size = game.world_size
         self.world_surface = pygame.Surface(self.world_size)
+        self.grid = GridGenerator(self.map_size).genMap()
+        
+        self.tiles = []
+        
         for x in range(self.map_size[0]):
             for y in range(self.map_size[1]):
-                self.setTile(x,y)
+                if self.grid[x][y]:
+                    #impassible
+                    tile = Entity('impassabletile',{'x':x*self.tile_size[0],'y':y*self.tile_size[1]})
+                else:
+                    #passable
+                    tile = Entity('passabletile',{'x':x*self.tile_size[0],'y':y*self.tile_size[1]})
+                self.tiles.append(tile)
+        
+        for t in self.tiles:
+            t.handle('draw-tiles',self.world_surface)
 
         pygame.image.save(self.world_surface,"file.png")
 
@@ -25,22 +39,6 @@ class Render:
 #        self.cameras = [Camera(500,500) for m in xrange(4)]
         self.draw_surface = self.world_surface.copy()
 
-
-    def setTile(self,x,y):
-        value = randint(0,1)#self.game.map[x][y]
-        value = self.game.grid[x][y]
-        if value == 0:
-            pygame.draw.rect(\
-                    self.world_surface,(255,0,0),\
-                    (self.tile_size[0] * x,self.tile_size[1] * y\
-                    ,self.tile_size[0],self.tile_size[1])\
-                    )
-        elif value == 1:
-            pygame.draw.rect(\
-                    self.world_surface,(0,255,0),\
-                    (self.tile_size[0] * x,self.tile_size[1] * y\
-                    ,self.tile_size[0],self.tile_size[1])\
-                    )
 
     def render(self):
         offx = int(self.screen_size[0] / 2)
@@ -55,7 +53,6 @@ class Render:
         rect.center = self.cameras[3].pos()
         self.screen.blit(self.draw_surface,(offx,offy),rect)
         pygame.display.flip()
-        pygame.image.save(self.draw_surface,"file.png")
         self.draw_surface = self.world_surface.copy()
 
 
