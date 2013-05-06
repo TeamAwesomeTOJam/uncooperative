7066df44045df427403ab97ce972ec3784585b62
Nishkala Balakrishnan <entropic.nish@gmail.com>
2013-05-05 20:18:56 -0400

    DAMN YOUUUU

--- uncooperative/engine/game.py
+++ uncooperative/engine/game.py
@@ -123,12 +123,12 @@ class Game(object):
 
         self.items = []
         for i in self.item_names:
-        #    x_pos = (self.world_size[0]/self.world_rooms[0] * randint(0, self.world_rooms[0]-1)) + self.world_size[0]/self.world_rooms[0]/2
-        #    y_pos = (self.world_size[1]/self.world_rooms[1] * randint(0, self.world_rooms[1]-1)) + self.world_size[1]/self.world_rooms[1]/2
-            self.items.append(Entity(i)) #, properties={
-        #        "x": x_pos,
-        #        "y": y_pos
-        #    }))
+            x_pos = (self.world_size[0]/self.world_rooms[0] * randint(0, self.world_rooms[0]-1)) + self.world_size[0]/self.world_rooms[0]/2
+            y_pos = (self.world_size[1]/self.world_rooms[1] * randint(0, self.world_rooms[1]-1)) + self.world_size[1]/self.world_rooms[1]/2
+            self.items.append(Entity(i, properties={
+                "x": x_pos,
+                "y": y_pos
+            }))
         
         self.splash_screen = Entity('splashscreen')
         self.screen.blit(self.resource_manager.get('sprite',self.splash_screen.props.image),(0,0))
